555e25c0df47cdc3d240ef2d89597daa4efc5f1d
尹祺霖 <22371157@buaa.edu.cn>
2024-05-22 21:18:48 +0800

    333

--- user/lib/strace.c
+++ user/lib/strace.c
@@ -17,8 +17,32 @@ void strace_send(int sysno) {
 	}
 
 	// Your code here. (1/2)
+	if(straced == 0){
+	return;
+		}
+	int temMark = straced;
+	straced = 0;
+	u_int nowId = syscall_getenvid();
+	int parentid = envs[ENVX(nowId)].env_parent_id;
+	ipc_send(parentid,sysno,0,0);
+	straced = 0;
+	syscall_set_env_status(nowId,ENV_NOT_RUNNABLE);
+	straced = temMark;	
 }
 
 void strace_recv() {
 	// Your code here. (2/2)
+	while(1){
+	u_int child_env_id;
+	u_int temPerm;
+	u_int temValue = ipc_recv(&child_env_id,0,&temPerm);
+		
+	strace_barrier(child_env_id);
+	recv_sysno(child_env_id, temValue);
+	syscall_set_env_status(child_env_id,ENV_RUNNABLE);
+	
+	if(temValue == SYS_env_destroy){
+	break;
+		}
+	}
 }
